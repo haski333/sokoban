3d9c78c80e1c5479ad5675c93f2f06997bb9c42e
rares31 <72224989+haski333@users.noreply.github.com>
2023-04-11 21:33:28 +0300

    Update sokoban.lua

--- sokoban.lua
+++ sokoban.lua
@@ -1,13 +1,3 @@
-local owner = "Upbolt"
-local branch = "revision"
-
-local function webImport(file)
-	return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
-end
-
-webImport("init")
-webImport("ui/main")
-
 local Sokoban = {}
 
 local RNG = Random.new()
